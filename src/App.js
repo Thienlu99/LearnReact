7d60ae57044a9a513f3e0dd740dab807d7e57f1e
Lê Thanh Thiên <107629259+Thienlu99@users.noreply.github.com>
2022-12-27 10:18:38 +0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
 // import './App.css';
+import { BrowserRouter,Link,Route,Routes } from 'react-router-dom';
 import Photo from './features/Photo';
 import Todo from './features/Todo';
 
 function App() {
   return (
     <div className="App">
-       <Photo />
-       <Todo />
+     <BrowserRouter>
+       <h2>Header</h2> 
+       <Link to="/todos">Todo</Link>
+       <br />
+       <Link to="/album">Album</Link>
+
+       <Routes>
+       {/* <Route path='/' element={<Todo />} exact/> */}
+          <Route path='/todos' element={<Todo />} />
+          <Route path='/album' element={<Photo />}/>
+       </Routes>
+       <h2>Footer</h2>
+       </BrowserRouter>
+
     </div>
   );
 }
